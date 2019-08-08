f389c323b98990bfd4e85e6dda4e853d57f17bc8
Jenwng <Jenniescwong@gmail.com>
2019-08-08 14:23:20 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,18 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-console.log("User flipped" + " " + cardOne);
+cards = ["queen", "queen", "king", "king"];
+cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+for (i = 0; i < cardsInPlay.length; i++) {
+	console.log("User flipped" + " " + cardsInPlay[i]);
+}
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
