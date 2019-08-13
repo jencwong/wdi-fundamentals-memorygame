cddc95e739667fe8a7c66ecb8fe47d01a5b33f97
Jenwng <Jenniescwong@gmail.com>
2019-08-13 13:56:36 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,26 @@
-cards = ["queen", "queen", "king", "king"];
+cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png",
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png", 
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png",
+}
+];
+
 cardsInPlay = [];
 
 function checkForMatch(){
@@ -6,20 +28,24 @@ function checkForMatch(){
 		if (cardsInPlay[0] === cardsInPlay[1]) {
 			console.log("You found a match!");
 		} else {
-			console.log("Sorry, try again.");
+			alert("Sorry, try again.");
 		};
 	};
 };
 
 
 function flipCard(cardId){
-	console.log("User flipped" + " " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped" + " " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 	checkForMatch();
 };
 
 flipCard(0);
 flipCard(2);
+
+
 
 
 
